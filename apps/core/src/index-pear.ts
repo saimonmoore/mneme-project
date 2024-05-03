0a9a332b24159189bc2966be38cad1daa0efa29c
Saimon Moore <saimonmoore@gmail.com>
2024-05-03 20:58:31 +0200

    Adding working pear index with repl

--- apps/core/src/index-pear.ts
+++ apps/core/src/index-pear.ts
@@ -1,12 +1,15 @@
 // @ts-ignore
-import readline from 'bare-readline'  // Module for reading user input in terminal
+import process from 'process'  // Module for reading user input in terminal
 // @ts-ignore
-import tty from 'bare-tty'            // Module to control terminal behavior
+import readline from 'readline'  // Module for reading user input in terminal
+// @ts-ignore
+import tty from 'tty'            // Module to control terminal behavior
 
 import { Mneme } from "@/Mneme/Mneme.js";
 import { User } from "@/modules/User/domain/entities/User.js";
 import { Record } from "@/modules/Record/domain/entities/Record.js";
-// // import { MnemeServer } from "@/server";
+import { Friend } from "@/modules/Friend/domain/entities/Friend.js";
+// import { MnemeServer } from "@/server";
 import { env, config, teardown } from "@/pear-compat";
 
 // @ts-ignore
@@ -29,30 +32,20 @@ if (!isTestRunning) {
     console.log(`Debug with pear://runtime/devtools/${key.toString("hex")}`);
   }
 
-  const rl = readline.createInterface({
-    input: new tty.ReadStream(0),
-    output: new tty.WriteStream(1),
-  });
-
-  rl.prompt();
-
   // @ts-ignore
-  const args = Pear.config.args.reduce((acc, curr, idx) => {
+  const args = Pear ? Pear.config.args.reduce((acc, curr, idx) => {
     if (idx % 2 === 0) {
       // @ts-ignore
       acc[curr.slice(1)] = Pear.config.args[idx + 1];
     }
     return acc;
-  }, {});
-  console.log(`Starting pear with args...`, { args});
+  }, {}) : {};
   const bootstrapPrivateCorePublicKey = args.bootstrap;
   const storage = args.storage || "./data";
 
   console.log("Starting Mneme with args", { storage, bootstrapPrivateCorePublicKey });
 
   mneme = new Mneme(bootstrapPrivateCorePublicKey, storage);
-
-  teardown(() => mneme.destroy())
 
   mneme.info();
 
@@ -63,16 +56,51 @@ if (!isTestRunning) {
   // @ts-ignore
   // server.start(mneme);
 
-  rl.on('line', (line) => {
-    try {
-      eval(line.trim());
-    } catch (err) {
-      console.error(err);
+  const rl = readline.createInterface({
+      input: new tty.ReadStream(0),
+      output: new tty.WriteStream(1),
+      prompt: 'mneme> ',
+  });
+  
+  // @ts-ignore
+  rl.input.setMode(tty.constants.MODE_RAW);
+  rl.on('data', (line: string) => {
+    switch (line.trim()) {
+      case '?':
+        console.log();
+        console.log('Just type an expression and I will evaluate it for you.');
+        console.log();
+        console.log('Type ".exit" to exit.');
+        break;
+      case '.exit':
+        console.log();
+        console.log('Have a great day!');
+        teardown(() => mneme.destroy())
+        process.exit(0);
+        break;
+     default:
+       // eslint-disable-next-line no-case-declarations
+       console.log();
+       // eslint-disable-next-line no-case-declarations
+       let result;
+       try {
+        result = eval(line.trim())
+       } catch (error) {
+         console.log("Error: ", error);
+       }
+       console.log(result);
+       break;
     }
     rl.prompt();
-  });
+  }).on('close', () => {
+    console.log();
+    console.log('Have a great day!');
+    teardown(() => mneme.destroy())
+    process.exit(0);
+  }); 
+  rl.prompt();
 }
 
-export { Mneme, User, Record };
+export { Friend, Mneme, Record, User };
 
 export { mneme };
