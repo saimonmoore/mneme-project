9de7ab82f6c43d492118b0073d07bb92a9e19ff9
Saimon Moore <saimonmoore@gmail.com>
2024-08-09 23:16:14 +0200

    feat(desktop): wip: mneme integration

--- apps/desktop/src/modules/Main/Main.tsx
+++ apps/desktop/src/modules/Main/Main.tsx
@@ -6,20 +6,63 @@ import { LoggedOutLayout } from "@mneme/desktop/ui/viewComponents/layout/LoggedO
 import { LoginSignupPage } from "@mneme/desktop/ui/page/LoginSignup/LoginSignupPage";
 import { Page } from "@mneme/desktop/ui/core/Page/Page";
 import { Dashboard } from "@mneme/desktop/ui/page/Dashboard/DashboardPage";
+import { DHT, useDHT, RAM } from '@mneme/hyper-web';
+import { Mneme } from '@mneme/core';
+import { useEffect } from "react";
+
+const Dummy = () => {
+  const { dht } = useDHT();
+  const listeners = [
+    {
+      event: Mneme.EVENTS.USER_LOGIN, callback: (user: User) => {
+        console.log("info: You are now logged in...", { user: user.email });
+        console.log();
+        console.log(
+          "info: Use the following key to synchronise Mneme to your other devices: ",
+          this.outOfBandSyncKey
+        );
+      }
+    },
+    {
+      event: Mneme.EVENTS.MNEME_READY, callback: () => {
+        console.log("info: Mneme is ready for business!");
+      }
+    },
+  ];
+
+  useEffect(() => {
+    if (!dht) return;
+    console.log('=========> GOT DHT', { dht });
+
+    async function startMneme() {
+      const mneme = new Mneme(undefined, RAM, undefined, dht, listeners);
+      await mneme.start();
+      console.log('=========> GOT Mneme', { mneme });
+    }
+
+    startMneme();
+
+  }, [dht]);
+
+  return <div>DHT</div>;
+}
 
 export const Main = () => {
   const queryClient = new QueryClient();
   const currentUser = useMnemeStore((state) => state.currentUser);
   return (
-    <QueryClientProvider client={queryClient}>
-      <Page>
-        {!currentUser && <LoggedOutLayout>{<LoginSignupPage />}</LoggedOutLayout>}
-        {currentUser && (
-          <LoggedInLayout>
-            <Dashboard />
-          </LoggedInLayout>
-        )}
-      </Page>
-    </QueryClientProvider>
+    <DHT>
+      <QueryClientProvider client={queryClient}>
+        <Page>
+          <Dummy />
+          {!currentUser && <LoggedOutLayout>{<LoginSignupPage />}</LoggedOutLayout>}
+          {currentUser && (
+            <LoggedInLayout>
+              <Dashboard />
+            </LoggedInLayout>
+          )}
+        </Page>
+      </QueryClientProvider>
+    </DHT>
   );
 };
