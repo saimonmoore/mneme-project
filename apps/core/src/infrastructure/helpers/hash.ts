96e8cfb4be13da5d43a46c73d991ada7a712e901
Saimon Moore <saimonmoore@gmail.com>
2024-08-09 21:55:23 +0200

    fix(core): replace crypto with sodium-universal

--- apps/core/src/infrastructure/helpers/hash.ts
+++ apps/core/src/infrastructure/helpers/hash.ts
@@ -1,7 +1,14 @@
-// @ts-ignore
-import crypto from 'crypto';
+import b4a from 'b4a'
+//@ts-expect-error
+import sodium from 'sodium-universal'
+
 import type { Hash } from '@mneme/domain';
 
 export function sha256(input: string): Hash {
-  return crypto.createHash('sha256').update(input).digest('hex') as Hash;
+  const inputBuffer = b4a.from(input)
+  const outputBuffer = b4a.alloc(sodium.crypto_hash_sha256_BYTES)
+  
+  sodium.crypto_hash_sha256(outputBuffer, inputBuffer)
+  
+  return b4a.toString(outputBuffer, 'hex') as Hash;
 }
