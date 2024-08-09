48cae855aab7ae0eeed43c68d62e97260c6a1ad5
Saimon Moore <saimonmoore@gmail.com>
2024-08-09 23:14:57 +0200

    chore(desktop): allow adding loadEnv in vite config

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -1,27 +1,35 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import reactNativeWeb from "vite-plugin-react-native-web";
 // import createExternal from 'vite-plugin-external';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  resolve: {
-    alias: {
-      '@mneme/desktop': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  // const env = loadEnv(mode, process.cwd(), '');
+
+  return ({
+    base: './',
+    // define: {
+    //   'process.env.APP_ENV': JSON.stringify(env.VITE_AP_ENV),
+    // },
+    resolve: {
+      alias: {
+        '@mneme/desktop': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  plugins: [
-    react(
-      {
-        babel: {
-          parserOpts: {
-            plugins: ['decorators-legacy'],
+    plugins: [
+      react(
+        {
+          babel: {
+            parserOpts: {
+              plugins: ['decorators-legacy'],
+            },
           },
-        },
-      }
-    ),
-    reactNativeWeb(),
-  ],
-})
+        }
+      ),
+      reactNativeWeb(),
+    ],
+  })
+}
+);
