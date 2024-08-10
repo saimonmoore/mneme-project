1dfdc74d30b25e10bf039a133769caba8c4c1d34
Saimon Moore <saimonmoore@gmail.com>
2024-08-10 21:58:23 +0200

    fix(components): try to fix 'React does not recognize prop'.
    Should use transient properties...

--- packages/components/src/ui/core/Button/Button.tsx
+++ packages/components/src/ui/core/Button/Button.tsx
@@ -1,7 +1,4 @@
 import { Button as GluestackButton } from "@gluestack-ui/themed";
 import { styled } from "@gluestack-style/react";
 
-export const Button = styled(GluestackButton, {
-  // bg: "$primary600",
-  // pt: "$6",
-});
+export const Button = styled(GluestackButton);
--- packages/components/src/ui/core/Text/Text.tsx
+++ packages/components/src/ui/core/Text/Text.tsx
@@ -1,7 +1,4 @@
 import { Text as GluestackText } from "@gluestack-ui/themed";
 import { styled } from "@gluestack-style/react";
 
-export const Text = styled(GluestackText, {
-  // bg: "$primary600",
-  // pt: "$6",
-});
+export const Text = styled(GluestackText);
