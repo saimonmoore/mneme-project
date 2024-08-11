2efa0654d8e28f6cdd5d0a277a1fc9309fab1cd7
Saimon Moore <saimonmoore@gmail.com>
2024-08-11 14:18:52 +0200

    chore(core-web): use local dht relay
    rather thank Lukks

--- packages/core-web/src/config/constants.ts
+++ packages/core-web/src/config/constants.ts
@@ -1,1 +1,3 @@
-export const DHT_RELAY_ADDRESS = 'wss://dht1-relay.leet.ar:49443'
+export const PRODUCTION_DHT_RELAY_ADDRESS = 'wss://dht1-relay.leet.ar:49443'
+export const DEVELOPMENT_DHT_RELAY_ADDRESS = 'ws://localhost:49443'
+export const DHT_RELAY_ADDRESS = DEVELOPMENT_DHT_RELAY_ADDRESS
