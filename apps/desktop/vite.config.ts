31f9b18001992a997fc7a049e1c15bcb44f55b56
Saimon Moore <saimonmoore@gmail.com>
2024-08-12 19:07:41 +0200

    feat(build): add OPENAI_API_KEY env var to vite config

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -6,13 +6,13 @@ import reactNativeWeb from "vite-plugin-react-native-web";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '');
 
   return ({
     base: './',
-    // define: {
-    //   'process.env.APP_ENV': JSON.stringify(env.VITE_AP_ENV),
-    // },
+    define: {
+      'process.env.OPENAI_API_KEY': JSON.stringify(env.VITE_OPENAI_API_KEY),
+    },
     resolve: {
       alias: {
         '@mneme/desktop': path.resolve(__dirname, './src'),
