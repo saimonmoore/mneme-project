8bda1290a92d73160138e570e30386478c79b2c7
Saimon Moore <saimonmoore@gmail.com>
2024-08-12 19:06:08 +0200

    fix(core-web): use local dev address for p2p-proxy
    points to nginx running in docker image

--- packages/core-web/src/config/constants.ts
+++ packages/core-web/src/config/constants.ts
@@ -1,3 +1,3 @@
 export const PRODUCTION_DHT_RELAY_ADDRESS = 'wss://dht1-relay.leet.ar:49443'
-export const DEVELOPMENT_DHT_RELAY_ADDRESS = 'ws://localhost:49443'
+export const DEVELOPMENT_DHT_RELAY_ADDRESS = 'ws://localhost:80'
 export const DHT_RELAY_ADDRESS = DEVELOPMENT_DHT_RELAY_ADDRESS
