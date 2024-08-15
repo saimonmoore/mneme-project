779ccee87baf3ee7e67c9abc385d41404d9c3e63
Saimon Moore <saimonmoore@gmail.com>
2024-08-15 23:12:35 +0200

    chore(desktop): ensure yarn install for core
    before root yarn install (it's not part of the monorepo so needs to be
    done manually)

--- apps/desktop/bin/build-deps.sh
+++ apps/desktop/bin/build-deps.sh
@@ -6,6 +6,7 @@ pushd packages/domain/; yarn build && popd;
 pushd packages/ai/; yarn build && popd;
 pushd packages/core-web; yarn build && popd;
 pushd packages/components/; yarn build && popd;
+pushd apps/core/; yarn install && popd;
 
 # In root
 yarn install --force;
