a683d14407faa39196988fad0e32ad4f5e2e18f7
Saimon Moore <saimonmoore@gmail.com>
2024-08-17 15:42:19 +0200

    feat(desktop): integrate basic navigation
    Use react-navigation

--- apps/desktop/src/modules/Main/Main.tsx
+++ apps/desktop/src/modules/Main/Main.tsx
@@ -1,29 +1,39 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
 import { DHT, MnemeProvider } from '@mneme/core-web';
 import { useMnemeStore } from '@mneme/desktop/store';
-import { LoggedInLayout } from '@mneme/desktop/ui/viewComponents/layout/LoggedIn/LoggedInLayout';
-import { LoggedOutLayout } from '@mneme/desktop/ui/viewComponents/layout/LoggedOut/LoggedOutLayout';
 import { LoginSignupPage } from '@mneme/desktop/ui/page/LoginSignup/LoginSignupPage';
 import { Page } from '@mneme/desktop/ui/core/Page/Page';
 import { Dashboard } from '@mneme/desktop/ui/page/Dashboard/DashboardPage';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const AppRouter = () => {
+  const currentUser = useMnemeStore((state) => state.currentUser);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {!currentUser ? (
+          <Stack.Screen name="LoginSignup" component={LoginSignupPage} />
+        ) : (
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export const Main = () => {
   const queryClient = new QueryClient();
-  const currentUser = useMnemeStore((state) => state.currentUser);
+
   return (
     <DHT>
       <MnemeProvider>
         <QueryClientProvider client={queryClient}>
           <Page>
-            {!currentUser && (
-              <LoggedOutLayout>{<LoginSignupPage />}</LoggedOutLayout>
-            )}
-            {currentUser && (
-              <LoggedInLayout>
-                <Dashboard />
-              </LoggedInLayout>
-            )}
+            <AppRouter />
           </Page>
         </QueryClientProvider>
       </MnemeProvider>
